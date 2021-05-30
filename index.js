28bd21ecb59b0712d6a50322a1d48cdcb7d9bef2
SaintJul <favor.julius@gmail.com>
2021-05-30 16:30:30 -0400

    commit tree b259df1385eae6734bd4ff7eb503b446604736e3

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name1,mail){
+    const formData = {
+        name: name1 ,
+        email: mail
+    }
+    const configObject = { 
+        method : "POST", 
+        headers : {
+            "Content-Type": "application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify(formData) 
+        
+    }
+    return fetch("http://localhost:3000/users", configObject)
+    
+    .then(function (response){
+        return response.json();
+    })
+    .then(function(object){
+        console.log(object);
+        document.body.innerHTML = object["id"]        
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+
+
+      
+}
